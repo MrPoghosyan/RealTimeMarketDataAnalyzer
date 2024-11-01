9e764c99546f0bae2d01e7c94853ea0239755921
MrPoghosyan <installation.v.p@gmail.com>
2024-11-01 20:08:36 +0400

    Add file: DataAnalyzer.cpp & DataAnalyzer.h

---
+++ DataAnalyzer.h
@@ -0,0 +1,13 @@
+// DataAnalyzer.h
+#ifndef DATAANALYZER_H
+#define DATAANALYZER_H
+
+#include <string>
+
+class DataAnalyzer {
+public:
+    void analyze(const std::string& filename);  // Method to analyze data from file
+};
+
+#endif // DATAANALYZER_H
+
